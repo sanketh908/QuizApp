42b130594759c970ce34de88699992ec8357ddd3
sanketh908 <sankeths908@gmail.com>
2026-03-02 07:20:35 +0530

    Added new Quiz controller

--- src/main/java/com/Sanketh/quizapp/Controller/QuizController.java
+++ src/main/java/com/Sanketh/quizapp/Controller/QuizController.java
@@ -1,4 +1,17 @@
 package com.Sanketh.quizapp.Controller;
 
+import com.Sanketh.quizapp.Entity.Question;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("quiz")
 public class QuizController {
+    @PostMapping("create")
+    public ResponseEntity<String> createQuiz(@RequestBody String category,
+                                             @RequestParam int numQ,
+                                             @RequestParam String title) {
+
+
+    }
 }
---
+++ src/main/java/com/Sanketh/quizapp/Service/QuizeService.java
@@ -0,0 +1,7 @@
+package com.Sanketh.quizapp.Service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class QuizeService {
+}
