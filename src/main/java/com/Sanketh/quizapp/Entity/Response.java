a895c6b2946cf1d2eeb3c993640ff5b943b39ef8
sanketh908 <sankeths908@gmail.com>
2026-03-02 15:29:43 +0530

    new changers

--- src/main/java/com/Sanketh/quizapp/Entity/Response.java
+++ src/main/java/com/Sanketh/quizapp/Entity/Response.java
@@ -1,4 +1,18 @@
 package com.Sanketh.quizapp.Entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+@Entity
 public class Response {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String response;
 }
