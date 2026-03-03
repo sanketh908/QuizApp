147e9f23600005bcde28438f2ade577117ae5b4a
sanketh908 <sankeths908@gmail.com>
2026-03-03 20:32:20 +0530

    final

--- src/main/java/com/Sanketh/quizapp/Entity/Question.java
+++ src/main/java/com/Sanketh/quizapp/Entity/Question.java
@@ -10,7 +10,7 @@ import lombok.Data;
 @Entity
 public class Question {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String questionText;
     private String optionA;
