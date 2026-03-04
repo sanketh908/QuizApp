bead9e0deae5b82ab1e662d1a265b7ad5b8482db
sanketh908 <sankeths908@gmail.com>
2026-03-04 16:03:36 +0530

    Added user

--- src/main/java/com/Sanketh/quizapp/Controller/AdminController.java
+++ src/main/java/com/Sanketh/quizapp/Controller/AdminController.java
@@ -3,9 +3,12 @@ package com.Sanketh.quizapp.Controller;
 import com.Sanketh.quizapp.Entity.User;
 import com.Sanketh.quizapp.Repository.UserRepository;
 import com.Sanketh.quizapp.Service.AdminService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/admin")
@@ -19,6 +22,11 @@ public class AdminController
     public User addAdmin(User user)
     {
         return adminService.addAdmin(user);
+    }
+    @GetMapping("/getallusers")
+    public List<User> getAllUsers()
+    {
+        return adminService.findAll();
     }
 
 }
