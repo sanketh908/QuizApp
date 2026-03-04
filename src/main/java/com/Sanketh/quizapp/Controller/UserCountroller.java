cd0bcb34dd27a34f8b1e80ffabad5829a1d8006f
sanketh908 <sankeths908@gmail.com>
2026-03-04 15:27:29 +0530

    Added user

--- src/main/java/com/Sanketh/quizapp/Controller/UserCountroller.java
+++ src/main/java/com/Sanketh/quizapp/Controller/UserCountroller.java
@@ -1,20 +1,33 @@
 package com.Sanketh.quizapp.Controller;
 
 import com.Sanketh.quizapp.Entity.User;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.Sanketh.quizapp.Service.UserService;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
 
 public class UserCountroller {
-    UserS
-    @PostMapping("/adduser")
-    public String addUser(@RequestBody User user) {
-        user.setRole("USER");
 
+  private final   UserService userService;
+
+    public UserCountroller(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/adduser")
+    public User addUser(@RequestBody User user) {
+        user.setRole("USER");
+      return   userService.saveUser(user);
+
+    }
+    @GetMapping("/getUser/{id}")
+    public User getUser(@PathVariable int id) {
+       return userService.findById(id);
+    }
+    @GetMapping("/getUser/{username}")
+    public User getUser(@PathVariable String username) {
+        return userService.findByUsername(username);
     }
 
 }
