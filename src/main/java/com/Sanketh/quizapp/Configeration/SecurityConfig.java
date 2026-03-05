f2a24a3d2c6f0b1776ed4917ceb978d16de7bd89
sanketh908 <sankeths908@gmail.com>
2026-03-05 06:30:09 +0530

    Added user

--- src/main/java/com/Sanketh/quizapp/Configeration/SecurityConfig.java
+++ src/main/java/com/Sanketh/quizapp/Configeration/SecurityConfig.java
@@ -1,19 +1,14 @@
 package com.Sanketh.quizapp.Configeration;
 
-import com.Sanketh.quizapp.Entity.User;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-
-import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -22,16 +17,18 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        http
+     return    http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/quiz/**").permitAll()
                         .requestMatchers("/questions/**","/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
-                .httpBasic(Customizer.withDefaults());
+                .httpBasic(Customizer.withDefaults())
+             .sessionManagement(session -> session.sessionCreationPolicy(org.springframework.security.config.http.SessionCreationPolicy.STATELESS))
+                .build();
 
-        return http.build();
+
     }
     @Bean
     public PasswordEncoder passwordEncoder() {
